ed479496d34cdbe57220c82ed4481f8ffdc6e8f8
Akharam <gerichk14@gmail.com>
2017-01-18 15:57:43 +0100

    version fin du cours 1
    J'en suis au filtre

--- roles/index.php
+++ roles/index.php
@@ -1,47 +1,84 @@
 <?php
 require_once "../connexion.php";
 $db = creerConnexionBD();
+?>
 
+<FORM method='post'>
+    <fieldset>
+        <legend> Role :</legend>
+        <label>Nom : </label><input type='text' name='fiNom' />
+    <INPUT TYPE='submit' NAME='filtre' VALUE='Filtrer'><br>
 
+        <table border=1>
+            <tr><td>Nom des roles</td><td>Nombre d'utilisateurs</td></tr>    
 
-echo "Nom des roles : <br>";
+    <?php
+            
+if(isset($_POST['filtre']) && !empty($_POST['filtre'])){
+    echo $_POST['filtre'];
+    $filtre = "where name ='".$_POST['filtre']."'";
+    var_dump($filtre);
+}
+else
+    $filtre ="";
 
 if(isset($_POST['triCrois'])){
-    
-$sql = "select * from role order by name";
+$sql = "select count(*) as nb, name from user us 
+join role ro on ro.id=us.idrole ".$filtre."
+group by name order by name";
     $reponse = $db->query($sql);
     foreach($reponse as $nom){
-        echo "-> ".$nom['name']."<br>";
+        echo "<tr><td> ".$nom['name']."</td><td> ".$nom['nb']."</td></tr><br>";
     }  
 }
 else if(isset($_POST['triDecrois'])){
-     $sql = "select * from role order by name desc";
+    $sql = "select count(*) as nb, name from user us 
+join role ro on ro.id=us.idrole ".$filtre."
+group by name order by name desc";
     $reponse = $db->query($sql);
     foreach($reponse as $nom){
-        echo "-> ".$nom['name']."<br>";
+        echo "<tr><td> ".$nom['name']."</td><td> ".$nom['nb']."</td></tr><br>";
+    }
+    
+}
+else
+    if(isset($_POST['supNB'])){
+$sql = "select count(*) as nb, name from user us 
+join role ro on ro.id=us.idrole ".$filtre."
+group by name order by nb";
+    $reponse = $db->query($sql);
+    foreach($reponse as $nom){
+        echo "<tr><td> ".$nom['name']."</td><td> ".$nom['nb']."</td></tr><br>";
     }  
 }
-
-else{
-    $sql = "select * from role";
+else if(isset($_POST['infNB'])){
+    $sql = "select count(*) as nb, name from user us 
+join role ro on ro.id=us.idrole ".$filtre."
+group by name order by nb desc";
     $reponse = $db->query($sql);
     foreach($reponse as $nom){
-        echo "-> ".$nom['name']."<br>";
+        echo "<tr><td> ".$nom['name']."</td><td> ".$nom['nb']."</td></tr><br>";
+    }
+    
+}
+else{
+    $sql = "select count(*) as nb, name from user us 
+join role ro on ro.id=us.idrole ".$filtre."
+group by name";
+    $reponse = $db->query($sql);
+    foreach($reponse as $nom){
+        echo "<tr><td> ".$nom['name']."</td><td> ".$nom['nb']."</td></tr><br>";
     }
 }
+echo "</tr>";
 
-    
 
-
-$sql = "select count(*) as nb from role";
-$reponse = $db->query($sql);
-$rep = $reponse->fetchColumn();
-
-echo "<br><br>";
-echo "Nombre de roles : ".$rep;
-
-echo "<FORM method='post'>";
-echo "<INPUT TYPE='submit' NAME='triCrois' VALUE='Tri croissant'>";
-echo "<INPUT TYPE='submit' NAME='triDecrois' VALUE='Tri décroissant'>";
-echo "</FORM>";
 ?>
+        </table>
+<INPUT TYPE='submit' NAME='triCrois' VALUE='Tri croissant'>
+<INPUT TYPE='submit' NAME='triDecrois' VALUE='Tri decroissant'>
+<INPUT TYPE='submit' NAME='supNB' VALUE='Nombre Croissant'>
+<INPUT TYPE='submit' NAME='infNB' VALUE='Nom decroissant'>
+
+
+</fieldset></FORM>
